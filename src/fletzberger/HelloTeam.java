deffb47caea227365241a30f75f059d5e073b5de
mfletzberger <mfletzberger@student.tgm.ac.at>
2018-02-13 12:12:47 +0100

    Hallo von Pirat Weiser!

---
+++ src/fletzberger/HelloTeam.java
@@ -0,0 +1,13 @@
+package fletzberger;
+
+public class HelloTeam {
+	public static void halloVonPiratWeiser() {
+		System.out.println("HARRWaschmachinensinddolHARR");
+	}
+
+	public static void main(String[] args) {
+		halloVonPiratWeiser();
+
+	}
+
+}
